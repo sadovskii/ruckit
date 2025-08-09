70a4fc8664d79c5c39c10569a6d6dd5be5e79528
Anton <anton.sadovsky.97@gmail.com>
2025-08-09 18:09:25 +0200

    inject html template to search

--- src/extension/black-list/button/black-list-button-ingestion.ts
+++ src/extension/black-list/button/black-list-button-ingestion.ts
@@ -1,9 +1,26 @@
+import { TEMPLATE } from "./black-list-button-templates";
+
 export class BlackListButtonIngestion {
     injectBlackListButtonOnSearch() {
-        document.querySelectorAll('ytd-video-renderer ytd-channel-name').forEach(channels => {
-            const channelNameElement = channels as HTMLElement;
-            if (channelNameElement) {
-                channelNameElement.style.backgroundColor = 'red';
+        const path = 'ytd-video-renderer #channel-info:not(:has(.black-list))';
+
+        document.querySelectorAll(path).forEach(channelInfo => {
+            const channelInfoElement = channelInfo as HTMLElement;
+            if (channelInfoElement) {
+                const thumbnail = channelInfoElement.querySelector('#channel-thumbnail');
+
+                if (thumbnail) {
+                    const thumbnailElement = thumbnail as HTMLElement;
+                    thumbnailElement.style.paddingRight = '6px';
+
+                    const newNode = document.createElement('div');
+                    newNode.innerHTML = TEMPLATE;
+
+                    const mainElement = newNode.firstElementChild as HTMLElement;
+                    mainElement.style.marginRight = '3.5px';
+
+                    thumbnailElement.insertAdjacentElement('afterend', mainElement);
+                }
             }
         });
     }

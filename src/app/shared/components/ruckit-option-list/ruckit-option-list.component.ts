ff39689c1c9a8befbfcc1c51c4c5d5e3021754d0
Anton <anton.sadovsky.97@gmail.com>
2025-08-16 12:05:32 +0200

    hide adding channel black list restriction from settings

--- src/app/shared/components/ruckit-option-list/ruckit-option-list.component.ts
+++ src/app/shared/components/ruckit-option-list/ruckit-option-list.component.ts
@@ -36,6 +36,9 @@ export class RuckitOptionListComponent implements OnInit, OnDestroy {
   public maxLengthRestriction = 200;
 
   @Input()
+  public enableAdding: boolean = true;
+
+  @Input()
   public height: string = '13.1rem';
 
   @Output()
@@ -74,7 +77,7 @@ export class RuckitOptionListComponent implements OnInit, OnDestroy {
 
   protected addItemClick() {
     this.invalidAfterClick = true;
-    if (this.searchFormControl.valid) {
+    if (this.searchFormControl.valid && this.enableAdding) {
       this.invalidAfterClick = false;
       this.addItem.emit(this.searchFormControl.value!);
       this.searchFormControl.setValue('');
